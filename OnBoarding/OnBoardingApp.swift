405ed4328a060ae68fee5dc43e98ae116aa576bc
kenmaro3 <miha.ken.19@gmail.com>
2023-07-08 11:02:53 +0900

    Initial Commit

---
+++ OnBoarding/OnBoardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoardingApp.swift
+//  OnBoarding
+//
+//  Created by Kentaro Mihara on 2023/07/08.
+//
+
+import SwiftUI
+
+@main
+struct OnBoardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
